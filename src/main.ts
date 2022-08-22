3621809c03979ffb802b076fea4b5ceb75b177ce
shy1118 <shy1118999@qq.com>
2022-08-22 17:07:38 +0800

    fix:nprogress not show

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@
  * @Author: shaohang-shy
  * @Date: 2022-08-11 11:04:24
  * @LastEditors: shaohang-shy
- * @LastEditTime: 2022-08-22 10:05:48
+ * @LastEditTime: 2022-08-22 17:06:13
  * @Description: main
  */
 import { createApp } from 'vue'
@@ -12,6 +12,7 @@ import App from './App.vue'
 import router from '~/router'
 import '@unocss/reset/tailwind.css'
 import './styles/main.css'
+import 'nprogress/nprogress.css' // 这个nprogress样式必须引入
 import 'uno.css'
 import '~/router/permissions'
 import 'virtual:svg-icons-register'
