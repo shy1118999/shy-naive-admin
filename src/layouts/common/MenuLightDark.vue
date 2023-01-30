424b81c35e66ba29e3ab6ac86e2514623cdb3118
shy1118 <shy1118999@qq.com>
2023-01-30 18:01:56 +0800

    fix:guide page dark mode

--- src/layouts/common/MenuLightDark.vue
+++ src/layouts/common/MenuLightDark.vue
@@ -2,17 +2,22 @@
  * @Author: shaohang-shy
  * @Date: 2022-08-18 20:03:11
  * @LastEditors: shaohang-shy
- * @LastEditTime: 2022-08-22 15:46:38
+ * @LastEditTime: 2023-01-30 17:48:54
  * @Description: MenuLightDark
 -->
 <script setup lang="ts">
 import { DarkModeRound, LightModeRound } from '@vicons/material'
+import { toggleDark } from '~/composables/dark'
 import { useAppStore } from '~/store'
 const appStore = useAppStore()
+function toggle() {
+  appStore.toggleTheme()
+  toggleDark()
+}
 </script>
 
 <template>
-  <n-icon size="30" @click="appStore.toggleTheme()">
+  <n-icon size="30" @click="toggle">
     <DarkModeRound v-if="appStore.theme === 'dark'" />
     <LightModeRound v-else />
   </n-icon>
--- src/pages/guide/index.vue
+++ src/pages/guide/index.vue
@@ -2,7 +2,7 @@
  * @Author: shaohang-shy
  * @Date: 2023-01-30 16:17:55
  * @LastEditors: shaohang-shy
- * @LastEditTime: 2023-01-30 17:34:11
+ * @LastEditTime: 2023-01-30 18:00:22
  * @Description: guide
 -->
 <script setup lang="ts">
@@ -21,10 +21,43 @@ function guide() {
 <template>
   <div class="p-3">
     <p class="bg-gray/20 py-2 px-3 mb-3">
-      引导页对于一些第一次进入项目的人很有用，你可以简单介绍下项目的功能。本 Demo 是基于 <a href="https://github.com/kamranahmedse/driver.js" target="_black" class="text-blue">driver.js.</a>
+      引导页对于一些第一次进入项目的人很有用，你可以简单介绍下项目的功能。本 Demo 是基于 <a
+        href="https://github.com/kamranahmedse/driver.js" target="_black"
+        class="text-blue"
+      >driver.js.</a>
     </p>
     <n-button @click.prevent.stop="guide">
       打开引导
     </n-button>
   </div>
 </template>
+
+<style>
+.dark #driver-highlighted-element-stage,
+.dark #driver-popover-item {
+  background-color: #333 !important;
+  color: #fff !important;
+}
+
+.dark #driver-popover-item .driver-popover-description {
+  color: #ddd;
+}
+
+.dark #driver-popover-item .driver-close-btn,
+.dark #driver-popover-item .driver-prev-btn,
+.dark #driver-popover-item .driver-next-btn {
+  background-color: #333 !important;
+}
+
+.dark #driver-popover-item .driver-popover-tip.top {
+  border-color: transparent transparent #333 !important;
+}
+
+.dark #driver-popover-item .driver-popover-tip.right {
+  border-color: transparent transparent transparent #333 !important;
+}
+.dark #driver-popover-item .driver-popover-footer button {
+  color: #fff;
+  text-shadow: 1px 1px 0 #000;
+}
+</style>
