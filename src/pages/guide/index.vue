90a5b79754e923d03f4f023eeabe5f8310c5c526
shy1118 <shy1118999@qq.com>
2023-01-30 17:37:01 +0800

    fix

--- src/pages/guide/index.vue
+++ src/pages/guide/index.vue
@@ -2,7 +2,7 @@
  * @Author: shaohang-shy
  * @Date: 2023-01-30 16:17:55
  * @LastEditors: shaohang-shy
- * @LastEditTime: 2023-01-30 16:40:59
+ * @LastEditTime: 2023-01-30 17:34:11
  * @Description: guide
 -->
 <script setup lang="ts">
@@ -20,6 +20,9 @@ function guide() {
 
 <template>
   <div class="p-3">
+    <p class="bg-gray/20 py-2 px-3 mb-3">
+      引导页对于一些第一次进入项目的人很有用，你可以简单介绍下项目的功能。本 Demo 是基于 <a href="https://github.com/kamranahmedse/driver.js" target="_black" class="text-blue">driver.js.</a>
+    </p>
     <n-button @click.prevent.stop="guide">
       打开引导
     </n-button>
